dd6abdcd4c2aafc330474bcea81799a5cd23ed0c
marcuxo <marcourrutiam@gmail.com>
2023-04-14 08:46:29 -0400

    change in URL

--- url/Url.js
+++ url/Url.js
@@ -1,3 +1,3 @@
-// export const URL_SRV = "https://apimedidores.apidev.info/app-medidor/"
+export const URL_SRV = "https://apimedidores.apidev.info/app-medidor/"
 
-export const URL_SRV = "http://10.30.11.23:4088/app-medidor/"
+// export const URL_SRV = "http://10.30.11.23:4088/app-medidor/"
